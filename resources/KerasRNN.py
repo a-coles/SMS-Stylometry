bc2500b363c67e1dc8e0f84917748390b9210ecc
Linozeross <lino@toranjenner.de>
2017-12-03 18:14:27 -0500

    Cleaned up the dev, test set generation Implemented in nb and for KerasRNN I have some issues with the random generator

--- resources/KerasRNN.py
+++ resources/KerasRNN.py
@@ -1,6 +1,7 @@
 
 import json
 import numpy as np
+from importData import importAndProcess
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from keras.preprocessing.text import text_to_word_sequence
@@ -8,36 +9,8 @@ from keras.preprocessing.text import Tokenizer
 from keras.utils import np_utils
 
 
-
-
-if __name__ == '__main__':
-    # Open up pre-processed location corpus
-    with open('english_sms_corpus.json', 'r') as fp:
-        obj = json.load(fp)
-
-    # Convert the loaded dictionary into pertinent lists
-    messages = []
-    locations = []
-    messages_locations = []
-    stop_words = set(stopwords.words("english"))
-    for message, location in obj.items():
-        messages.append(message)
-
-        # Clean up the location
-        if location.lower() == "singapore":
-            location = "sg"
-        elif location.lower() == "united states":
-            location = "us"
-        elif location.lower() == "united kingdom":
-            location = "uk"
-        locations.append(location)
-
-    # Convert these to a numpy array
-    messages = np.array(messages)
-    locations = np.array(locations)
-
-
-    #get single words out of sentences
+def preprocess(messages):
+    # get single words out of sentences
     tokMessages = []
 
     maxlength = 0
@@ -46,35 +19,51 @@ if __name__ == '__main__':
         tok = word_tokenize(msg)
         tokMessages.append(tok)
         if len(tok) > maxlength:
-            maxlength =len(tok)
+            maxlength = len(tok)
             longest = tok
 
-    newMsg =[]
+    newMsg = []
     for msg in tokMessages:
         n = " ".join(msg)
         newMsg.append(n)
+    return newMsg
 
+
+if __name__ == '__main__':
+    messages_train, loc_train, messages_dev, loc_dev, messages_test, loc_test = importAndProcess()
+
+
+    newMsg = preprocess(messages_train)
     toker = Tokenizer(filters = '')
     toker.fit_on_texts(newMsg)
     X_train = toker.texts_to_sequences(newMsg)
     print(len(toker.word_counts))
     X_train = np.asarray(X_train)
 
-    print (np.shape(tokMessages))
-    print(maxlength)
-    #print(" ".join (longest))
+    newMsg = preprocess(messages_dev)
+    X_dev = toker.texts_to_sequences(newMsg)
+    X_dev = np.asarray(X_dev)
 
-    print(np.shape(locations))
-    print(locations[2])
-    y_train = locations
-    y_train[locations == 'sg'] = 0
-    y_train[locations == 'us'] = 1
-    y_train[locations == 'uk'] = 2
+    newMsg = preprocess(messages_test)
+    X_test = toker.texts_to_sequences(newMsg)
+    X_test = np.asarray(X_test)
+
+    y_train = loc_train
+    y_train[loc_train == 'sg'] = 0
+    y_train[loc_train == 'us'] = 1
+    y_train[loc_train == 'uk'] = 2
+
+    y_dev = loc_dev
+    y_dev[loc_dev == 'sg'] = 0
+    y_dev[loc_dev == 'us'] = 1
+    y_dev[loc_dev == 'uk'] = 2
+
+    y_test = loc_test
+    y_test[loc_test == 'sg'] = 0
+    y_test[loc_test == 'us'] = 1
+    y_test[loc_test == 'uk'] = 2
 
 
-
-# LSTM for sequence classification in the IMDB dataset
-from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
@@ -82,18 +71,20 @@ from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 # fix random seed for reproducibility
 np.random.seed(43)
-# load the dataset but only keep the top n words, zero the rest
-#top_words = 5000
-
-
-#(X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=top_words)
-#print(X_train[1])
 
 num_classes = []
 # truncate and pad input sequences
+
 max_length = 200
 X_train = sequence.pad_sequences(X_train, maxlen=max_length)
+X_dev = sequence.pad_sequences(X_dev, maxlen=max_length)
+X_test = sequence.pad_sequences(X_test, maxlen=max_length)
+
 y_train = np_utils.to_categorical(y_train, num_classes)
+y_dev = np_utils.to_categorical(y_dev, num_classes)
+y_test = np_utils.to_categorical(y_test, num_classes)
+
+
 print(y_train)
 # create the model
 embedding_vector_length = 32
@@ -103,10 +94,10 @@ model.add(LSTM(100))
 model.add(Dense(3, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
-model.fit(X_train, y_train, nb_epoch=10, batch_size=64,verbose=1, validation_split=0.1, shuffle =True)
+model.fit(X_train, y_train, nb_epoch=10, batch_size=64,verbose=1, validation_data=(X_dev,y_dev), shuffle =True)
 # Final evaluation of the model
-#scores = model.evaluate(X_test, y_test, verbose=0)
-#print("Accuracy: %.2f%%" % (scores[1]*100))
+scores = model.evaluate(X_test, y_test, verbose=1)
+print("Accuracy: %.2f%%" % (scores[1]*100))
 
 
 
---
+++ resources/importData.py
@@ -0,0 +1,50 @@
+import json
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def importAndProcess():
+    np.random.seed(43)
+    #Split is train=0.8,dev=0.1,test=0.1
+    trainTest = 0.2
+    testDev = 0.5
+
+
+    # Open up pre-processed location corpus
+    with open('english_sms_corpus.json', 'r') as fp:
+        obj = json.load(fp)
+
+    # Convert the loaded dictionary into pertinent lists
+    messages = []
+    locations = []
+    messages_locations = []
+    for message, location in obj.items():
+        messages.append(message)
+
+        # Clean up the location
+        if location.lower() == "singapore":
+            location = "sg"
+        elif location.lower() == "united states":
+            location = "us"
+        elif location.lower() == "united kingdom":
+            location = "uk"
+        locations.append(location)
+
+    # Convert these to a numpy array
+    messages = np.array(messages)
+    locations = np.array(locations)
+
+    #TODO: Somehow I get different results every time, can you reproduce?
+    # With your old version it didn't happen but I don't know whats different
+
+    # Shuffle and split into train and test set
+    messages_train, messages_test, loc_train, loc_test = train_test_split(messages, locations, test_size=trainTest,random_state=43)
+
+    # Split the "test" set into development and test set
+    messages_dev, messages_test, loc_dev, loc_test = train_test_split(messages_test, loc_test, test_size=testDev,random_state=43)
+    return messages_train,loc_train,messages_dev,loc_dev,messages_test,loc_test
+
+
+
+
+
+
